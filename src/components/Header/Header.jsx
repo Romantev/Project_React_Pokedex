4fd9e48a697209456c60123788872168881a008a
Roman Parfentev <romanntev@gmail.com>
2023-06-20 09:55:24 +0200

    push from main

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,25 @@
 import { useState } from "react";
 import "./Header.css";
-import logo from "./pokemon.png"
+import logo from "./pokemon.png";
 import Nav from "../Nav/Nav";
 import SearchBar from "../SearchBar/SearchBar";
-import BtnDarkMode from "../BtnDarkMode/BtnDarkMode"
+import BtnDarkMode from "../BtnDarkMode/BtnDarkMode";
 
 const Header = () => {
-
   const [icon, setIcon] = useState(false);
 
-  return (<>
-
-    <header className="header">
-      <div className="img-wrapper">
-        <img src={logo} alt="Pokemon-Logo"/>
-      </div>
-      <Nav />
-      <SearchBar />
-      <BtnDarkMode />
-    </header>
-  
-  </>);
+  return (
+    <>
+      <header className="header">
+        <div className="img-wrapper">
+          <img src={logo} alt="Pokemon-Logo" />
+        </div>
+        <Nav />
+        <SearchBar />
+        {/* <BtnDarkMode /> */}
+      </header>
+    </>
+  );
 };
 
 export default Header;
