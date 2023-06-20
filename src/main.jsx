7b7fffacb5075513885cc37ac89bad962302e9b2
Quaselz <paul.geppert94@freenet.de>
2023-06-20 09:06:40 +0200

    fonts css hinzugefügt

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './fonts/RobotoSlab-Regular.ttf'
+import './fonts/Roboto-Black.ttf'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
