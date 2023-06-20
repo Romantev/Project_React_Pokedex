7fbd6231b79b5179b86a08e56291a395e86d0596
Roman Parfentev <romanntev@gmail.com>
2023-06-20 14:05:39 +0200

    edit DarkMode

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import "./App.css";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import { DarkModeContext } from "./context/Context";
 
 import Home from "./pages/Home/Home";
 import DetailSite from "./pages/detailsite/Detailsite";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/detail-site/:id" element={<DetailSite />} />
-        </Routes>
-      </BrowserRouter>
+      <DarkModeContext.Provider value={{ darkMode, setDarkMode }}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/detail-site/:id" element={<DetailSite />} />
+          </Routes>
+        </BrowserRouter>
+      </DarkModeContext.Provider>
     </>
   );
 }
